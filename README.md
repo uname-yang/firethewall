4ff7a77facf62ee007cd7db22105babe17b9b9b1
Yu <Y.Yu@thomsonreuters.com>
2018-06-21 17:32:26 +0800

    README

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # firethewall
+
+> 科学上网、搬梯子工具
+
+## 我是如何上Youtube的
+分享一个我的翻墙方法。首先要到国外租一台服务器，然后用ssh建立连接的同时，在本地做一个映射的代理端口。最后配置本机代理服务，让所有流量走代理端口。这样做就相当于本机与墙外的服务器建立一个加密隧道，所有的网络连接都是从外网的服务器发出与取回，再传回到国内的主机。
+
+这样做的好处是根本不会被封掉，到外网的通讯都是非对称加密，GFW应该不会发现。这个项目就是为了简化我整个上网过程而建立的。假设是没有固定的服务器在国外。脚本功能如下：
+* 使用DigitalOcean的API创建主机，配制SSH密钥等
+* 获取建立ssh隧道连接的信息
+* 销毁云上的主机，不用时可以停掉服务器，减少花费
+
+## Hankbook
+
+### 1. 注册DigitalOcean账号
+推荐使用 [DigitalOcean](https://www.digitalocean.com/?refcode=cc0edbf24578) 的服务。
+DigitalOcean是一家建立于美国的云基础架构提供商，面向软件开发人员提供虚拟专用服务器（VPS）。该公司总部设在纽约市，并在世界各地拥有数据中心。2015年12月，DigitalOcean成为了全球第二大面向Web的网络寄存服务公司。
+使用这个链接注册可以获得$10的使用额度。最低资源的服务器为$5一个月，可以使用2个月。不用上网时可以删掉主机，节省费用，$10可以用很久。
+```
+https://www.digitalocean.com/?refcode=cc0edbf24578
+```
+
+### 2. 配置Token和登陆密钥
+
+登陆到DigitalOcean控制台后，依次点击[API]->[Generate New Token]，把创建的Token保存好。然后点击[右上角个人图标]->[Settings]->[Security]->[Add SSH key],将公钥上传。最后将公钥的Fingerprint同样保存好。
+如何生成ssh密钥对可以按照[Github的文档](https://help.github.com/articles/connecting-to-github-with-ssh/)。
+```
+https://help.github.com/articles/connecting-to-github-with-ssh/
+```
+### 3. 添加验证信息到脚本
+
+打开编辑脚本：
+```python
+TOKEN = "XXX"
+FINGERPRINT = "XXX"
+```
+将上一步保存下来的Token和Fingerprint替换XXX。
+
+### 4. 运行脚本
+
+1. 创建主机
+```
+python3 firethewall.py connect
+```
+2. 获取主机连接信息并建立ssh隧道，这里需要根据提示手动执行ssh命令
+```
+python3 firethewall.py status
+```
+3. 使用完后，销毁主机
+```
+python3 firethewall.py destroy
+```
+
+### 5. 配置本地代理,以MacOS为例
+依次 [Setting]->[Network]->[Advance]->[Proxies]
+添加SOCK Proxy如图，端口号要和上一步执行的参数一致，ssh隧道建立后再将选线勾上。使用完，同样要将代理配置去除。否则上不了网。
+![img](img/setting.png)
+
+## END
+现在就可以正常浏览网站了。如果遇到网络连接丢失之类的问题，可以先停掉本地代理，使用新的端口号重新建立连接，再更改本地代理配置到新端口。
+
+欢迎指正！
--- firethewall.py
+++ firethewall.py
@@ -8,21 +8,22 @@ import sys
 TOKEN = "XXX"
 FINGERPRINT = "XXX"
 
-#EXAMPLE
+# EXAMPLE
 #TOKEN = "6b360de366ab91d38e1dc153cb650f6297182998418cddca433f223af32497cf"
 #FINGERPRINT = "6b:2c:c2:8b:84:c4:2d:19:d1:a1:c5:88:24:3f:11:c1"
 
 IDENT = "sshtun66"
 
+
 def createServer():
     headers = {"Content-Type": "application/json",
-               "Authorization": "Bearer "+TOKEN}
+               "Authorization": "Bearer " + TOKEN}
 
     url = "https://api.digitalocean.com/v2/droplets"
-    
+
     payload = {"name": IDENT, "region": "nyc3", "size": "s-1vcpu-1gb", "image": "ubuntu-17-10-x64",
                "ssh_keys": [FINGERPRINT], "tags": ["net-bridge"]}
-    
+
     try:
         r = requests.post(url, headers=headers, data=json.dumps(payload))
         data = json.loads(r.text)
@@ -36,13 +37,13 @@ def createServer():
 def deleteServer(sid):
     # print(sid)
     headers = {"Content-Type": "application/json",
-               "Authorization": "Bearer "+TOKEN}
+               "Authorization": "Bearer " + TOKEN}
 
-    url = "https://api.digitalocean.com/v2/droplets/"+str(sid)
+    url = "https://api.digitalocean.com/v2/droplets/" + str(sid)
 
     try:
         requests.delete(url, headers=headers)
-        print("Drop server["+str(sid)+"]")
+        print("Drop server[" + str(sid) + "]")
     except Exception:
         logging.error(Exception)
         raise
@@ -50,7 +51,7 @@ def deleteServer(sid):
 
 def listServer():
     headers = {"Content-Type": "application/json",
-               "Authorization": "Bearer "+TOKEN}
+               "Authorization": "Bearer " + TOKEN}
 
     url = "https://api.digitalocean.com/v2/droplets"
 
@@ -58,7 +59,7 @@ def listServer():
         r = requests.get(url, headers=headers)
         data = json.loads(r.text)
 
-        drops={"droplets":[]}
+        drops = {"droplets": []}
 
         for droplet in data['droplets']:
             if droplet['name'] == IDENT:
@@ -74,33 +75,38 @@ def status():
     # print(listServer())
     for droplet in listServer()['droplets']:
         # print(droplet)
-        print("server["+str(droplet['id'])+"] with ip:"+droplet['networks']['v4'][0]['ip_address'])
+        print("server[" + str(droplet['id']) + "] with ip:" +
+              droplet['networks']['v4'][0]['ip_address'])
         print("\t please run command `ssh root@" +
-              droplet['networks']['v4'][0]['ip_address']+" -N -f -D 1088` in shell")
+              droplet['networks']['v4'][0]['ip_address'] + " -N -f -D 1088` in shell")
     return 0
+
 
 def connect():
     if len(listServer()['droplets']) > 0:
         status()
     else:
         id = createServer()
-        print("creating server ["+str(id)+"]")
+        print("creating server [" + str(id) + "]")
         # status()
     return 0
+
 
 def destroy():
     for droplet in listServer()['droplets']:
         deleteServer(droplet['id'])
     return 0
 
+
 def help_text():
     print("available options:   connect|status|destroy")
+
 
 if __name__ == '__main__':
 
     command = 'help'
 
-    if len(sys.argv)>=2:
+    if len(sys.argv) >= 2:
         command = sys.argv[1]
 
     if command == 'help':
@@ -111,6 +117,6 @@ if __name__ == '__main__':
 
     if command == 'connect':
         connect()
-    
+
     if command == 'destroy':
         destroy()
